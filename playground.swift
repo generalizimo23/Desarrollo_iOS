3a2c50383eb968a831cf0ddbe4ee6cd5ee50551b
generalizimo23 <43912121+generalizimo23@users.noreply.github.com>
2021-08-18 12:56:09 -0700

    Create playground.swift

---
+++ playground.swift
@@ -0,0 +1,6 @@
+var v1 = "esto es Swift";
+let c1 = 4; 
+var cadena:String = String();
+cadena = "Hola mundo" + " " + v1;
+print("El codigo es: \(cadena)");  
+print("El valor de constante es: \(c1)"); 
